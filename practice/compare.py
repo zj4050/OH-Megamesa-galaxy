43ef3dd49b6146bed75cfdd93077de5ce617ad09
Z. Ji <99605082+zj4050@users.noreply.github.com>
2023-03-02 16:17:37 -0500

    Delete practice directory

--- practice/compare.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-file1 = '/Users/kamiori/Desktop/Python/Research Project/OH Megamesa/line_fitting/fit_data/iras11-239-p15_1_PPXFfit.txt'
-file2 = '/Users/kamiori/Desktop/Python/Research Project/OH Megamesa/line_fitting/fit_data/iras11-239-p15_PPXFfit.txt'
-
-df1 = np.loadtxt(file1, skiprows= 1)
-df2 = np.loadtxt(file2, skiprows=1)
-
